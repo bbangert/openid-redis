31742e54ce4fdd54820b66326275939907cab112
Ben Bangert <ben@groovie.org>
2010-04-28 19:40:30 -0700

    Adding license/readme files. Adding bare bones section of actual backend.

--- openidredis/__init__.py
+++ openidredis/__init__.py
@@ -1,1 +1,86 @@
-#
+from openid import cryptutil
+from openid import oidutil
+from openid.association import Association
+from openid.store import nonce
+from openid.store.interface import OpenIDStore
+
+import redis
+
+__all__ = ['RedisStore']
+
+_filename_allowed = string.ascii_letters + string.digits + '.'
+_isFilenameSafe = set(_filename_allowed).__contains__
+
+def _safe64(s):
+    h64 = oidutil.toBase64(cryptutil.sha1(s))
+    h64 = h64.replace('+', '_')
+    h64 = h64.replace('/', '.')
+    h64 = h64.replace('=', '')
+    return h64
+
+def _filenameEscape(s):
+    filename_chunks = []
+    for c in s:
+        if _isFilenameSafe(c):
+            filename_chunks.append(c)
+        else:
+            filename_chunks.append('_%02X' % ord(c))
+    return ''.join(filename_chunks)
+
+
+class RedisStore(OpenIDStore):
+    """Implementation of OpenIDStore for Redis"""
+    def __init__(self, host='localhost', port=6379, db=0):
+        self.host = host
+        self.port = port
+        self.db = db
+        self._conn = redis.Redis(host=self.host, port=self.port, db=self.db)
+    
+    def getAssociationFilename(self, server_url, handle):
+        """Create a unique filename for a given server url and
+        handle. This implementation does not assume anything about the
+        format of the handle. The filename that is returned will
+        contain the domain name from the server URL for ease of human
+        inspection of the data directory.
+
+        (str, str) -> str
+        """
+        if server_url.find('://') == -1:
+            raise ValueError('Bad server URL: %r' % server_url)
+
+        proto, rest = server_url.split('://', 1)
+        domain = _filenameEscape(rest.split('/', 1)[0])
+        url_hash = _safe64(server_url)
+        if handle:
+            handle_hash = _safe64(handle)
+        else:
+            handle_hash = ''
+
+        filename = '%s-%s-%s-%s' % (proto, domain, url_hash, handle_hash)
+        return filename
+
+    def storeAssociation(self, server_url, association):
+        """We use a bunch of the"""
+        association_s = association.serialize()
+        key_name = self.getAssociationFilename(server_url, association.handle)
+        self._conn.set(key_name, association_s)
+        
+        # By default, set the expiration from the assocation expiration
+        self._conn.expire(key_name, association.lifetime)
+    
+    def getAssociation(self, server_url, handle=None):
+        key_name = self.getAssociationFilename(server_url, handle)
+        if handle is None:
+            handle = ''
+        association_s = self._conn.get(key_name)
+        if association_s:
+            return Association.deserialize(association_s)
+        else:
+            return None
+    
+    def removeAssociation(self, server_url, handle):
+        key_name = self.getAssociationFilename(server_url, handle)
+        return self._conn.del(key_name)
+    
+    def useNonce(self, server_url, timestamp, salt):
+        
