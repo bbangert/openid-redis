f1d342f32505af0c4b42abd322aa9547a6f08ef4
ThiefMaster <adrian@planetcoding.net>
2012-09-02 17:39:50 +0200

    Support redis connections with a password

--- openidredis/__init__.py
+++ openidredis/__init__.py
@@ -46,7 +46,8 @@ def _filenameEscape(s):
 class RedisStore(OpenIDStore):
     """Implementation of OpenIDStore for Redis"""
     def __init__(self, host='localhost', port=6379, db=0,
-            key_prefix='oid_redis', conn=None, unix_socket=None):
+            key_prefix='oid_redis', conn=None, unix_socket=None,
+            password=None):
         if conn is not None:
             self._conn = conn
             try:
@@ -58,12 +59,14 @@ class RedisStore(OpenIDStore):
                 self.port = 0
                 self.unix_socket = self._conn.connection_pool.connection_kwargs['path']
             self.db = self._conn.connection_pool.connection_kwargs['db']
+            self.password = self._conn.connection_pool.connection_kwargs['password']
         else:
             self.unix_socket = unix_socket
             self.host = host
             self.port = port
             self.db = db
-            redis_args = {'db': db}
+            self.password = password
+            redis_args = {'db': db, 'password': password}
             if self.unix_socket:
                 redis_args['unix_socket_path'] = self.unix_socket
             else:
