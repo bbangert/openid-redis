635208f138017bc7731020a74ff2b7c850fac0ba
Carlo Cabanilla <carlo.cabanilla@gmail.com>
2013-04-19 15:06:23 -0300

    Remove explicit dependencies
    Since we already manage those elsewhere

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(name='openid-redis',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
-      install_requires=["redis>=2.4.0", "python-openid>=2.2.5"],
       entry_points="""
       # -*- Entry points: -*-
       """,
